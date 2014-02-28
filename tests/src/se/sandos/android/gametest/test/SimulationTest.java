d95a3fdad81b8f3c3d729304d0c3914f6858a21e
John Bäckstrand <sopues@gmail.com>
2014-02-28 11:21:22 +0100

    Massive testN test

--- tests/src/se/sandos/android/gametest/test/SimulationTest.java
+++ tests/src/se/sandos/android/gametest/test/SimulationTest.java
@@ -4,7 +4,12 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Random;
+import java.util.Set;
 
 import junit.framework.Assert;
 import junit.framework.AssertionFailedError;
@@ -119,7 +124,7 @@ public class SimulationTest extends android.test.ActivityUnitTestCase<MainActivi
  				sim1.clicked();
  			}
  			
- 			Assert.assertNotSame(-1, sim2.compareHistory(sim1, 10));
+ 			Assert.assertEquals(-1, sim2.compareHistory(sim1, 10));
  			
  			try {
  				step(sim1, sim2, 5, x > 100, 1.0f);
@@ -140,7 +145,7 @@ public class SimulationTest extends android.test.ActivityUnitTestCase<MainActivi
  				sim1.clicked();
  			}
  			
- 			Assert.assertNotSame(-1, sim2.compareHistory(sim1, 10));
+ 			Assert.assertEquals(-1, sim2.compareHistory(sim1, 10));
  			
  			if(x > ITERATIONS*0.9) {
  				Assert.assertTrue("Simulation drift: " + sim1.timestep() + "|" + sim2.timestep(), Math.abs(sim1.timestep() - sim2.timestep()) < 5);
@@ -155,11 +160,122 @@ public class SimulationTest extends android.test.ActivityUnitTestCase<MainActivi
  		}
 	}
 	
-	public void testDelayLossComplexDuplex2Peers() throws UnknownHostException, IOException
+	public void testOutOfOrderPackets() throws UnknownHostException, IOException
 	{
 		GameSimulation sim1 = new GameSimulation(act, "sim1");
 		GameSimulation sim2 = new GameSimulation(act, "sim2");
 		
+		BinaryMessage bm1 = new BinaryMessage();
+		BinaryMessage bm2 = new BinaryMessage();
+		BinaryMessage temp = new BinaryMessage();
+		
+		sim1.clicked();
+		stepSynched(sim1, sim2, temp);
+		stepSynched(sim1, sim2, temp);
+		stepSynched(sim1, sim2, temp);
+		
+		sim1.clicked();
+		
+		stepSynched(sim1, sim2, temp);
+		
+		sim1.serialize(bm1);
+		bm2.parseFrom(bm1.getWritten());
+		sim2.absorb(bm2, addr);
+
+		//This packet is delayed...
+		sim2.serialize(bm2);
+
+		sim1.step();
+		sim2.step();
+		
+		stepSynched(sim1, sim2, temp);
+		stepSynched(sim1, sim2, temp);
+		stepSynched(sim1, sim2, temp);
+		
+		bm1.parseFrom(bm2.getWritten());
+		sim1.absorb(bm1, addr);
+		
+		sim1.step();
+		sim2.step();
+
+		sim1.step();
+		sim2.step();
+
+		stepSynched(sim1, sim2, temp);
+		stepSynched(sim1, sim2, temp);
+
+		
+	}
+	
+	public void testN() throws IOException
+	{
+		Map<GameSimulation, ArrayDeque<BinaryMessage>> sims = new HashMap<GameSimulation, ArrayDeque<BinaryMessage>>();
+		
+		for(int count=3; count<10; count++) {
+			sims.clear();
+			for(int j=0; j<count; j++) {
+				sims.put(new GameSimulation(act, "sim" + count), new ArrayDeque<BinaryMessage>(10));
+			}
+			
+			for(int x=0; x<ITERATIONS; x++) {
+				//Clicker
+				if(x < 200) {
+					int index = 0;
+					for(GameSimulation gs : sims.keySet()) {
+						index++;
+						if((index+x)%20 == 2) {
+							gs.clicked();
+						}
+					}
+				}
+			
+				Set<GameSimulation> done = new HashSet<GameSimulation>();
+				for(GameSimulation gs : sims.keySet()) {
+					for(GameSimulation peer : sims.keySet()) {
+						if(done.contains(peer)) {
+							continue;
+						}
+			 			Assert.assertEquals("Histories diverge at " + gs.timestep() + "|" + peer.timestep() + " Count is " + count + " " + gs + " " + peer, -1, peer.compareHistory(gs, 30));
+					}
+					done.add(gs);
+				}
+
+				
+				//Send network data
+				for(Entry<GameSimulation, ArrayDeque<BinaryMessage>> e : sims.entrySet()) {
+					GameSimulation s = e.getKey();
+					ArrayDeque<BinaryMessage> queue = e.getValue();
+
+					BinaryMessage bm = new BinaryMessage();
+					s.serialize(bm);
+					queue.add(bm);
+					
+					if(queue.size() >= 5) {
+						BinaryMessage poll = queue.poll();
+						Set<GameSimulation> others = sims.keySet();
+						others.remove(s);
+						for(GameSimulation gs : others) {
+							BinaryMessage b = new BinaryMessage();
+							b.parseFrom(poll.getWritten());
+							gs.absorb(b, addr);
+						}
+					}
+				}
+				
+				//Step
+				for(GameSimulation gs : sims.keySet()) {
+					gs.step();
+				}
+				
+			}
+		}
+	}
+	
+	public void testDelayLossComplexDuplex2Peers() throws UnknownHostException, IOException
+	{
+		GameSimulation sim1 = new GameSimulation(act, "sim1");
+		GameSimulation sim2 = new GameSimulation(act, "sim2");
+			
  		for(int x=0; x<ITERATIONS; x++) {
  			if(x < 300) {
 	 			if(x % 10 == 2) {
