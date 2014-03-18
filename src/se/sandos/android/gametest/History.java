4e2d8d9decb08612e1fdc5b49e492abffe30c5d2
John Bäckstrand <sopues@gmail.com>
2014-03-18 12:55:20 +0100

    Support for rewinding history and more tests

--- src/se/sandos/android/gametest/History.java
+++ src/se/sandos/android/gametest/History.java
@@ -1,7 +1,9 @@
 package se.sandos.android.gametest;
 
 import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.Deque;
+
+import android.util.Log;
 
 /**
  * History class with two different density arrays
@@ -11,32 +13,48 @@ import java.util.Queue;
  * @param <T>
  */
 public class History<T> {
-	private Queue<T> hQ;
+	private Deque<T> hQ;
 	private int hSize;
 	private int stride;
 	
-	private Queue<T> lQ;
+	private Deque<T> lQ;
 	private int lSize;
 	private int lTimestamp = -1;
 
 	// Timestamp at end
 	private int timestamp = -1;
 
+	private T empty;
+	
 	public History(Class<T> clz, int highDensitySize, int lowDensitySize,
-			int lowDensityStride) {
+			int lowDensityStride, T empty) {
 		stride = lowDensityStride;
 		hQ = new ArrayDeque<T>();
 		lQ = new ArrayDeque<T>();
 		
 		hSize = highDensitySize;
 		lSize = lowDensitySize;
+		this.empty = empty;
 	}
 
 	public int size() {
 		return hQ.size() + lQ.size();
 	}
 
+	public int getTimestamp() {
+		return timestamp;
+	}
+	
 	public void addNewRecord(T t, int ts) {
+		if(ts <= timestamp) {
+			Log.w("History", "Adding over old data");
+			return;
+		}
+		//Add null data...
+		while(timestamp != -1 && timestamp != ts-1) {
+			hQ.offer(empty);
+			timestamp++;
+		}
 		hQ.offer(t);
 		timestamp = ts;
 		if(hQ.size() > hSize) {
@@ -47,6 +65,10 @@ public class History<T> {
 				lTimestamp = ts - hSize;
 			} else {
 				int newTS = ts - hQ.size();
+				while(newTS > lTimestamp + stride) {
+					lQ.offer(empty);
+					lTimestamp += stride;
+				}
 				if(newTS == lTimestamp + stride) {
 					lQ.offer(removed);
 					lTimestamp = newTS;
@@ -54,6 +76,10 @@ public class History<T> {
 						lQ.poll();
 					}
 				}
+			}
+			//Clean out any empties
+			while(hQ.peek() == empty) {
+				hQ.poll();
 			}
 		}
 	}
@@ -71,6 +97,48 @@ public class History<T> {
 			} else {
 				return -1;
 			}
+		}
+	}
+	
+	public T rewind(int ts) {
+		if(oldestHistory() > ts || (timestamp != -1 && ts > timestamp)) {
+			return null;
+		}
+		
+		//We do a "simple" rollback, we never re-mix low-density and high-density queues again:
+		//It is simply not possible to recreate the high-density data from low-density data
+		//We simply "remove" the data that is too new
+		if(timestamp == -1 || timestamp - hQ.size() >= ts) {
+			hQ.clear();
+			timestamp = -1;
+			
+			if(lQ.isEmpty()) {
+				lTimestamp = -1;
+				return null;
+			}
+			
+			while(lTimestamp > ts) {
+				lQ.pollLast();
+				lTimestamp -= stride;
+			}
+			lTimestamp -= stride;
+			return lQ.pollLast();
+		} else {
+			while(timestamp > ts && !hQ.isEmpty()) {
+				hQ.pollLast();
+				timestamp--;
+			}
+			
+			while(hQ.peekLast() == empty) {
+				timestamp--;
+				hQ.pollLast();
+			}
+			T r = hQ.pollLast();
+			timestamp--;
+			if(hQ.size() == 0) {
+				timestamp = -1;
+			}
+			return r;
 		}
 	}
 }
--- tests/src/se/sandos/android/gametest/test/HistoryTest.java
+++ tests/src/se/sandos/android/gametest/test/HistoryTest.java
@@ -6,7 +6,7 @@ import se.sandos.android.gametest.History;
 
 public class HistoryTest extends TestCase {
 	public void testFill() {
-		History<int[]> h = new History<int[]>(int[].class, 5, 2, 3);
+		History<int[]> h = new History<int[]>(int[].class, 5, 2, 3, new int[1]);
 		
 		checkAge(-1, 0, h);
 		
@@ -27,11 +27,75 @@ public class HistoryTest extends TestCase {
 		addCheck(7, 15, 7, h);
 		addCheck(7, 16, 7, h);
 		addCheck(7, 17, 7, h);
-
 	}
 
+	public void testRewind() {
+		History<int[]> h = new History<int[]>(int[].class, 5, 2, 3, new int[1]);
+		
+		checkAge(-1, 0, h);
+		
+		addCheck(1, 1, 1, h);
+		addCheck(1, 2, 2, h);
+		addCheck(1, 3, 3, h);
+		addCheck(1, 4, 4, h);
+		
+		int[] rewind = h.rewind(2);
+		Assert.assertEquals(2, rewind[0]);
+	}
+	
+	public void testRewindLow() {
+		History<int[]> h = new History<int[]>(int[].class, 5, 2, 3, new int[1]);
+		
+		checkAge(-1, 0, h);
+		
+		addCheck(1, 1, 1, h);
+		addCheck(1, 2, 2, h);
+		addCheck(1, 3, 3, h);
+		addCheck(1, 4, 4, h);
+		addCheck(1, 5, 5, h);
+		addCheck(1, 6, 6, h);
+		addCheck(1, 7, 6, h);
+		addCheck(1, 8, 6, h);
+		addCheck(1, 9, 7, h);
+		
+		int[] rewind = h.rewind(4);
+		Assert.assertEquals(4, rewind[0]);
+		
+		rewind = h.rewind(3);
+		Assert.assertEquals(1, rewind[0]);
+
+		rewind = h.rewind(1);
+		Assert.assertNull(rewind);
+	}
+	
+	public void testPerformance() {
+		History<int[]> h = new History<int[]>(int[].class, 5, 30, 5, new int[1]);
+		int timestamp = 1;
+		int counter = 1;
+		for(int i=0; i<500000; i++) {
+			int[] t = new int[1];
+			t[0] = timestamp;
+			h.addNewRecord(t, timestamp);
+			if((counter % 31) == 0) {
+				if((counter % 124) == 0) {
+					int[] rewind = h.rewind(timestamp - 7);
+					Assert.assertTrue(timestamp - 7 >= rewind[0]);
+					timestamp -= 7;
+				} else {
+					int[] rewind = h.rewind(timestamp - 3);
+					Assert.assertTrue(timestamp - 3 >= rewind[0]);
+					timestamp -= 3;
+				}
+			}
+			timestamp++;
+			counter++;
+		}
+	}
+	
 	private void addCheck(int age, int ts, int size, History<int[]> h) {
-		h.addNewRecord(new int[1], ts);
+		int[] t = new int[1];
+		t[0] = ts;
+		h.addNewRecord(t, ts);
 		checkAge(age, size, h);
 	}
 	
